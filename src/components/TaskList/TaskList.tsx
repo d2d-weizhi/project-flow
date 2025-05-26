f24a4983633f0cba0b6dc86497d0bf87cbb9e59c
Chen Weizhi <164135380+d2d-weizhi@users.noreply.github.com>
2025-05-26 11:37:30 +0800

    Testing the dependency update

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -52,7 +52,7 @@ export default function TaskList({theme}: {theme: string}) {
       dispatch({ type: "FETCH_TASKS", payload: fetchedTasks });
     };
     fetchTasks();
-  }, [dispatch]); // Add dispatch to the dependency array
+  }, [dispatch, state]); // Add dispatch to the dependency array
 
   const handleCreateTask = async (newTask: ITask) => {
     try {
